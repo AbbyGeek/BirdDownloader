da4e567a331a1da44ac22fb1c983460aeaf578ce
Abby <abbyrosewest@gmail.com>
2023-08-22 16:05:01 -0400

    rearranged methods. Added reminder in main method for where I left off.

--- main.py
+++ main.py
@@ -3,25 +3,16 @@ import requests
 import os
 
 '''
-APPROACH
--import module
--create image folder
-get main page html code
-create list of all pages to navigate to in order to retrieve images
-navigate to each page in list
-download specific image
+ToDo:
+method to navigate to bird page and download image into correct folder
 '''
-#create image folder
-# folder_name = "Bird Images"
-# try:
-#     os.mkdir(folder_name)
-# except:
-#     print("Folder by desired name already exists")
-
 #get main page html code
 def main(url):
     soup = parsePage(url)
-    getLinksAndNames(soup)
+    namesDict = getLinksAndNames(soup)
+    photoGrabber(namesDict)
+    print("In the create folder method, there needs to be a way to return a dictionary with 'h2 name': [name, name, name]")
+    print("pass this dictionary into the photodownloader method so it can match up the bird names w/ the correct folder they're supposed to be in")
 
 def parsePage(url):
      #content of URL
@@ -31,7 +22,7 @@ def parsePage(url):
     return soup
 
 def getLinksAndNames(soup):
-    headingCount = 0
+    nameDict = {}
     #Find div's w/ div-col class
     for item in soup.find_all("div", {"class": "mw-parser-output"}):
         #Grab all lists and h2 tags
@@ -46,10 +37,12 @@ def getLinksAndNames(soup):
             #handle list items
             if item.name == 'li':
                 try:
-                    print(item.text)
-                    print('https://en.wikipedia.org/' + item.find('a', href=True)['href'])
+                    name = item.text
+                    url = 'https://en.wikipedia.org/' + item.find('a', href=True)['href']
+                    nameDict.update({name: url})
                 except:
                     print("Error with: "+item.text)
+    return nameDict
 
 
 def createFolders(headingName):
@@ -61,8 +54,39 @@ def createFolders(headingName):
     except:
         print("Folder named "+headingName+" alreay exists")
 
-def downloadPics (Name, href):
+def photoGrabber(namesDict):
+    for item in namesDict:
+        itemUrl = namesDict[item]
+        urlList = photoUrlGrabber(item, itemUrl)
+        photoDownloader(item, urlList)
+
+def photoUrlGrabber(item, itemUrl):
+    imageUrlList = []
+    #print name of item
+    #parse item page
+    itemSoup = parsePage(itemUrl)
+    #find table w/ main photos/info
+    itemTable = itemSoup.find(("table", {"class": "mw-parser-output"}))
+    anchors = itemTable.find_all('a')
+    for anchor in anchors:
+        images = anchor.find_all('img')
+        for image in images:
+            #if image is an icon (commonly the first anchor following main pic(s)), or map.
+            #this should be cleaned up
+            source = image['src']
+            if source == "//upload.wikimedia.org/wikipedia/commons/thumb/8/8a/OOjs_UI_icon_edit-ltr.svg/15px-OOjs_UI_icon_edit-ltr.svg.png" or source.endswith("map.svg.png"): 
+                break
+            imageUrlList.append("http:" + source)
+    if len(imageUrlList) == 0:
+        #Need to handle when empty list is returned
+        print("No URLs found for " + item)
+    return(imageUrlList)
+
+def photoDownloader(item, urlList):
+    print(item)
     return
+
+
 
             
 
